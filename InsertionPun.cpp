dfcd16d86008d04c17971b867f6c33bdc76cbb5e
Santiago Salas Sotillo <112534601+SantiagoSalasucsp@users.noreply.github.com>
2023-01-20 09:16:52 -0500

    ejem

--- InsertionPun.cpp
+++ InsertionPun.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+using namespace std;
+
 int insertionSort(int arr[], int tam, int pos, int elem) {
 int* pun;
 pun=arr;
@@ -9,4 +12,28 @@ int array[tam];
       else 
         array[i]=*(pun+i);
 
-  };
+        return 0;
+
+  }}
+
+  void imprimir(const int arr[],const int tam)
+{
+    for(int i=0;i<tam;i++)
+    {
+        cout<<arr[i]<<endl;
+    }
+}
+
+
+
+
+int main ()
+{
+    int array[]={3,4,6,1};
+    insertionSort(array,4,1,8);
+    imprimir(array,4);
+
+    
+
+
+}
